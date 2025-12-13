8fb6c611e42c107b0f96a48f3d08d3000de7f77e
mahmouddattiaa <119110428+mahmouddattiaa@users.noreply.github.com>
2025-12-13 03:22:36 +0200

    ss

---
+++ Forms/Assistant/MaintenanceForm.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace GetLab.Forms.Assistant
+    {
+    partial class MaintenanceForm
+        {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose ( bool disposing )
+            {
+            if ( disposing && ( components != null ) )
+                {
+                components.Dispose ( );
+                }
+            base.Dispose ( disposing );
+            }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ( )
+            {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dgvMaintenance = new System.Windows.Forms.DataGridView();
+            this.btnFix = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaintenance)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Cornsilk;
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(252, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Damaged Equipment List";
+            // 
+            // dgvMaintenance
+            // 
+            this.dgvMaintenance.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMaintenance.Location = new System.Drawing.Point(23, 51);
+            this.dgvMaintenance.Name = "dgvMaintenance";
+            this.dgvMaintenance.RowHeadersWidth = 62;
+            this.dgvMaintenance.RowTemplate.Height = 28;
+            this.dgvMaintenance.Size = new System.Drawing.Size(649, 234);
+            this.dgvMaintenance.TabIndex = 1;
+            // 
+            // btnFix
+            // 
+            this.btnFix.Location = new System.Drawing.Point(23, 291);
+            this.btnFix.Name = "btnFix";
+            this.btnFix.Size = new System.Drawing.Size(223, 65);
+            this.btnFix.TabIndex = 2;
+            this.btnFix.Text = "Mark Selected as Fixed";
+            this.btnFix.UseVisualStyleBackColor = true;
+            this.btnFix.Click += new System.EventHandler(this.btnFix_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(593, 378);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(101, 42);
+            this.btnClose.TabIndex = 3;
+            this.btnClose.Text = "close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // MaintenanceForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnFix);
+            this.Controls.Add(this.dgvMaintenance);
+            this.Controls.Add(this.label1);
+            this.Name = "MaintenanceForm";
+            this.Text = "MaintenanceForm";
+            this.Load += new System.EventHandler(this.MaintenanceForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMaintenance)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+            }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dgvMaintenance;
+        private System.Windows.Forms.Button btnFix;
+        private System.Windows.Forms.Button btnClose;
+        }
+    }
